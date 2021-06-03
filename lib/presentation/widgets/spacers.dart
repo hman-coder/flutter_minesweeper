ebdd64a627e0bd386f6a97b35b6f5cfe14883362
hman-coder <hman_codes@protonmail.com>
2021-06-03 16:08:03 +0300

    added constant spacer widgets

---
+++ lib/presentation/widgets/spacers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Widget kwTinyHorizontalSpacer = SizedBox(width: 10,);
+const Widget kwSmallHorizontalSpacer = SizedBox(width: 15,);
+const Widget kwMediumHorizontalSpacer = SizedBox(width: 20,);
+const Widget kwLargeHorizontalSpacer = SizedBox(width: 25,);
+const Widget kwEnormousHorizontalSpacer = SizedBox(width: 30,);
+
+const Widget kwTinyVerticalSpacer = SizedBox(height: 10,);
+const Widget kwSmallVerticalSpacer = SizedBox(height: 15,);
+const Widget kwMediumVerticalSpacer = SizedBox(height: 20,);
+const Widget kwLargeVerticalSpacer = SizedBox(height: 25,);
+const Widget kwEnormousVerticalSpacer = SizedBox(height: 30,);
