ba4ffa4f32b4bc6e5b8d8cd94de24d7c319f87d4
hman-coder <hman_codes@protonmail.com>
2021-06-20 07:09:44 +0300

    Added toggle audio

--- lib/bloc/audio_manager.dart
+++ lib/bloc/audio_manager.dart
@@ -2,13 +2,13 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:minesweeper_flutter/constants/audio.dart';
 
 class AudioManager {
-  final AudioCache _cache;
-
   static final AudioManager _instance = AudioManager._();
 
   factory AudioManager() {
     return _instance;
   }
+
+  final AudioCache _cache;
 
   AudioManager._() : _cache = AudioCache(prefix: ksAssetsPrefix) {
     _init();
@@ -16,7 +16,10 @@ class AudioManager {
 
   void _init() {
     _cache.load(ksNormalClick);
+    _cache.load(ksToggle);
   }
 
   void normalClick() => _cache.play(ksNormalClick);
+
+  void toggle() => _cache.play(ksToggle);
 }
--- lib/constants/audio.dart
+++ lib/constants/audio.dart
@@ -1,3 +1,5 @@
 const String ksAssetsPrefix = "assets/audio/";
 
 const String ksNormalClick = "normal_click.wav";
+
+const String ksToggle = "toggle.wav";
