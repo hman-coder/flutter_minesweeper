5f5e02b1cc6641a147bc525e3ddd729f4b54baf2
hman-coder <hman_codes@protonmail.com>
2021-06-20 05:12:12 +0300

    Multibloc provider is now used.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minesweeper_flutter/bloc/minesweeper_theme.dart';
 import 'package:minesweeper_flutter/constants/routes.dart';
+import 'package:minesweeper_flutter/repository/game_settings_repository.dart';
 import 'package:minesweeper_flutter/repository/minesweeper_theme_repository.dart';
 import 'package:minesweeper_flutter/constants/text.dart';
 import 'package:minesweeper_flutter/config/themes.dart';
 import 'package:minesweeper_flutter/config/route_generators.dart';
+import 'package:minesweeper_flutter/bloc/game_settings.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,14 +19,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  MinesweeperThemeBloc _createMinesweeperThemeBloc() =>
-      MinesweeperThemeBloc(MinesweeperSqliteThemeRepository())
-        ..add(ReloadThemeEvent());
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<MinesweeperThemeBloc>(
-      create: (context) => _createMinesweeperThemeBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MinesweeperThemeBloc>(
+            create: (context) =>
+                MinesweeperThemeBloc(MinesweeperThemeSqliteRepository())),
+        BlocProvider<GameSettingsBloc>(
+            create: (context) =>
+                GameSettingsBloc(GameSettingsSqliteRepository())..load()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
@@ -33,8 +38,8 @@ class _MyAppState extends State<MyApp> {
           fontFamily: kffFontFamily,
           textTheme: kttMainTextTheme,
           appBarTheme: kabtAppBarTheme,
+          
         ),
-
         onGenerateRoute: materialRouteGenerator,
         initialRoute: kprLoadingRoute,
       ),
