db28593fce8f150b2a6e76eeb33c51d38a81f5a6
hman-coder <hman_codes@protonmail.com>
2021-06-22 17:26:17 +0300

    Added toAppString extension on MinesweeperIcons

--- lib/presentation/icons/minesweeper_icons.dart
+++ lib/presentation/icons/minesweeper_icons.dart
@@ -15,13 +15,38 @@
 ///
 import 'package:flutter/widgets.dart';
 
+const _kFontFam = 'Minesweeper';
+
+const _kcpMineCodePoint = 0xe800;
+const _kcpFlagCodePoint = 0xe801;
+const _kcpMineRelaxedCodePoint = 0xe802;
+
 class MinesweeperIcons {
   MinesweeperIcons._();
 
-  static const _kFontFam = 'Minesweeper';
   static const String? _kFontPkg = null;
 
-  static const IconData mine = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData flag = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData mine_relaxed = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mine = IconData(_kcpMineCodePoint, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData flag = IconData(_kcpFlagCodePoint, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mine_relaxed = IconData(_kcpMineRelaxedCodePoint, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
+
+
+extension MinesweeperIconToString on IconData {
+  String toAppString() {
+    if(this.fontFamily == _kFontFam) {
+      switch(this.codePoint) {
+        case _kcpMineCodePoint:
+          return "mine";
+
+        case _kcpMineRelaxedCodePoint:
+          return "relaxed";
+
+        case _kcpFlagCodePoint:
+          return "flag";
+      }
+    }
+
+    throw Exception("This icon data is not of MinesweeperIcons.");
+  }
 }
