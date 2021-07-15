5fa900533a3df11a3edb783eb0783c62a464e521
hman-coder <hman_codes@protonmail.com>
2021-07-15 14:08:44 +0300

    Changed how MinesweeperLevelSettings work
    * GameMode and GameDifficulty are now enums.
    * MinesweeperLevelSettings has a default constrcutor.
    * In addition, it has a fromDifficulty constructor.
    * Many extensions has been added to the enums.
    * Added appropriate localizations.

--- lib/model/minesweeper_level_settings.dart
+++ lib/model/minesweeper_level_settings.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:minesweeper_flutter/helpers/context_extensions.dart';
+
 const String kkMinesKey = 'mines';
 const String kkHeightKey = 'height';
 const String kkWidthKey = 'width';
@@ -9,25 +12,12 @@ class MinesweeperLevelSettings {
 
   final int mines;
 
-  const MinesweeperLevelSettings.beginner({
-    this.height = 10,
-    this.width = 10,
-    this.mines = 10,
-  });
+  MinesweeperLevelSettings.fromDifficulty(GameDifficulty difficulty)
+      : height = difficulty.height,
+        width = difficulty.width,
+        mines = difficulty.mines;
 
-  const MinesweeperLevelSettings.intermediate({
-    this.height = 16,
-    this.width = 13,
-    this.mines = 40,
-  });
-
-  const MinesweeperLevelSettings.expert({
-    this.height = 30,
-    this.width = 16,
-    this.mines = 99,
-  });
-
-  const MinesweeperLevelSettings.custom({
+  const MinesweeperLevelSettings({
     required this.height,
     required this.width,
     required this.mines,
@@ -39,7 +29,7 @@ class MinesweeperLevelSettings {
         this.mines = map[kkMinesKey];
 
   MinesweeperLevelSettings copyWith({int? height, int? width, int? mines}) {
-    return MinesweeperLevelSettings.custom(
+    return MinesweeperLevelSettings(
       height: height ?? this.height,
       width: width ?? this.width,
       mines: mines ?? this.mines,
@@ -52,5 +42,162 @@ class MinesweeperLevelSettings {
     map[kkHeightKey] = this.height;
     map[kkWidthKey] = this.width;
     return map;
+  }
+}
+
+enum GameMode { standard, endless, run, multiplayer }
+
+extension ModeStrings on GameMode {
+  String toAppString(BuildContext context) {
+    switch (this) {
+      case GameMode.standard:
+        return context.localization().standardGameMode;
+
+      case GameMode.endless:
+        return context.localization().endlessGameMode;
+
+      case GameMode.run:
+        return context.localization().runGameMode;
+
+      case GameMode.multiplayer:
+        return context.localization().multiplayerGameMode;
+    }
+  }
+
+  String description(BuildContext context) {
+    switch (this) {
+      case GameMode.standard:
+        return context.localization().standardGameModeDescription;
+
+      case GameMode.endless:
+        return context.localization().endlessGameModeDescription;
+
+      case GameMode.run:
+        return context.localization().runGameModeDescription;
+
+      case GameMode.multiplayer:
+        return context.localization().multiplayerGameModeDescription;
+    }
+  }
+
+  String descriptionTitle(BuildContext context) {
+    switch (this) {
+      case GameMode.standard:
+        return context.localization().standardGameModeDescriptionTitle;
+
+      case GameMode.endless:
+        return context.localization().endlessGameModeDescriptionTitle;
+
+      case GameMode.run:
+        return context.localization().runGameModeDescriptionTitle;
+
+      case GameMode.multiplayer:
+        return context.localization().multiplayerGameModeDescriptionTitle;
+    }
+  }
+}
+
+extension ModeDifficulties on GameMode {
+  List<GameDifficulty> get difficulties {
+    switch (this) {
+      case GameMode.standard:
+        return GameDifficulty.values;
+
+      case GameMode.run:
+        return GameDifficulty.values.toList()..remove(GameDifficulty.custom);
+
+      case GameMode.endless:
+      case GameMode.multiplayer:
+        return [];
+    }
+  }
+}
+
+enum GameDifficulty { expert, hard, intermediate, easy, beginner, custom }
+
+extension DifficultySettings on GameDifficulty {
+  int get mines {
+    switch (this) {
+      case GameDifficulty.expert:
+        return 99;
+      case GameDifficulty.hard:
+        return 99;
+      case GameDifficulty.intermediate:
+        return 40;
+      case GameDifficulty.easy:
+        return 10;
+      case GameDifficulty.beginner:
+        return 10;
+      case GameDifficulty.custom:
+        return 0;
+    }
+  }
+
+  int get height {
+    switch (this) {
+      case GameDifficulty.expert:
+        return 30;
+
+      case GameDifficulty.hard:
+        return 30;
+
+      case GameDifficulty.intermediate:
+        return 16;
+
+      case GameDifficulty.easy:
+        return 10;
+
+      case GameDifficulty.beginner:
+        return 10;
+
+      case GameDifficulty.custom:
+        return 0;
+    }
+  }
+
+  int get width {
+    switch (this) {
+      case GameDifficulty.expert:
+        return 16;
+
+      case GameDifficulty.hard:
+        return 16;
+
+      case GameDifficulty.intermediate:
+        return 13;
+
+      case GameDifficulty.easy:
+        return 10;
+
+      case GameDifficulty.beginner:
+        return 10;
+
+      case GameDifficulty.custom:
+        return 0;
+    }
+  }
+}
+
+extension DiffcultyToString on GameDifficulty {
+  String toAppString(BuildContext context) {
+    switch (this) {
+      case GameDifficulty.expert:
+        return context.localization().expert;
+
+      case GameDifficulty.hard:
+        return context.localization().hard;
+
+      case GameDifficulty.intermediate:
+        return context.localization().intermediate;
+
+      case GameDifficulty.easy:
+        return context.localization().easy;
+
+      case GameDifficulty.beginner:
+        return context.localization().beginner;
+
+      case GameDifficulty.custom:
+        return context.localization().custom;
+    }
   }
 }
