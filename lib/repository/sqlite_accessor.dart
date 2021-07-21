937315189e3c1f9aa95253282d1cf20322b98405
hman-coder <hman_codes@protonmail.com>
2021-07-21 09:17:13 +0300

    Added an interface for accessors

--- lib/repository/sqlite_accessor.dart
+++ lib/repository/sqlite_accessor.dart
@@ -3,16 +3,48 @@ import 'package:minesweeper_flutter/constants/db_raw_statements.dart';
 
 const String kpSqliteDbPath = "sksminesweeperdb.db";
 
+/// An abstract implementation for a class that accesses
+/// some sort of data storage.
+abstract class Accessor {
+
+  /// Initializes the accessor. For example, in a sqlite db,
+  /// it opens the db file if it is closed.
+  _initialize();
+
+  /// Deletes all data, including both tables (keys) and values.
+  /// This is used mainly for debugging purposes.
+  void deleteAllData();
+
+  /// Prints the database with all its contents to the console.
+  /// This is used mainly for debugging purposes.
+  void printData();
+
+  /// Fetches all data with the given key
+  void fetch(String key);
+
+  /// Updates values for a given key.
+  /// [rowId] is typically omitted in non-sql accessors.
+  Future<int> update(String key, Map<String, dynamic> updatedColumnValues, int rowId);
+
+  /// Updates a settings value. This is different from the [update] method
+  /// in that it does not require a [rowId] param to operate, since there is a 
+  /// single value for each setting.
+  Future<int> updateSettings(String key, Map<String, dynamic> updatedColumnValues);
+
+  bool get _shouldInitialize;
+}
+
+
 /// This class is responsible for accessing the app's Sqlite database
 /// It exposes methods that repositories can use to manipulate the database.
-class SqliteAccessor {
+class SqliteAccessor extends Accessor{
   // Singleton implementation ----------------------------------
 
   static SqliteAccessor _accessor = SqliteAccessor._internal();
 
   static Future<SqliteAccessor> get accessor async {
-    if (_accessor._db == null || !_accessor._db!.isOpen) {
-      await _accessor._initializeDb();
+    if (_accessor._shouldInitialize) {
+      await _accessor._initialize();
     }
 
     return _accessor;
@@ -24,18 +56,23 @@ class SqliteAccessor {
 
   Database? _db;
 
-  _initializeDb() async {
+  @override
+  bool get _shouldInitialize => this._db == null || ! this._db!.isOpen;
+
+  @override
+  _initialize() async {
     _db = await openDatabase(
       kpSqliteDbPath,
       version: 1,
       onCreate: (db, version) async {
         await db.transaction((txn) async {
           await txn.execute(ksGameSettingsTableCreateStatement);
-          await txn.execute(ksMinesweeperLevelSettingsTableCreateStatement);
+          await txn.execute(ksLevelSettingsTableCreateStatement);
           await txn.execute(ksMinesweeperThemeTableCreateStatement);
           
           await txn.execute(ksMinesweeperThemeInitialDataInsertStatement);
           await txn.execute(ksGameSettingsInitialDataInsertStatement);
+          await txn.execute(ksLevelSettingsInitialDataInsertStatement);
         });
       },
       onUpgrade: (db, oldVersion, newVersion) => {
@@ -44,7 +81,8 @@ class SqliteAccessor {
     );
   }
 
-  void deleteDb() async {
+  @override
+  void deleteAllData() async {
     try {
       await deleteDatabase("${(await getDatabasesPath())}/$kpSqliteDbPath");
       print('deleted');
@@ -53,7 +91,8 @@ class SqliteAccessor {
     }
   }
 
-  void printDb() async {
+  @override
+  void printData() async {
     (await _db!.query('sqlite_master', columns: ['type', 'name'])).forEach((row) {
       print(row.values);
     });
